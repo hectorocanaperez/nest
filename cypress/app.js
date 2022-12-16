78ffbcd60aedffee19327eb1ebfd40f64acd265c
hectorocanaperez <102577133+hectorocanaperez@users.noreply.github.com>
2022-12-16 12:40:42 +0100

    pruebaPI

--- cypress/app.js
+++ cypress/app.js
@@ -4,6 +4,6 @@ describe('api', function() {
     it('api can be opened', function () {
       cy.visit('http://localhost:3003/transactions/todos')
       cy.contains('705ad8ae-a245-4f58-a394-05c96282b03b')
-      
+    
     })
   })
