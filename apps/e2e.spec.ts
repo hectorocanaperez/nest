05331c7f89187d3b2b399b193288012ac201c6bd
hectorocanaperez <102577133+hectorocanaperez@users.noreply.github.com>
2022-12-19 12:56:20 +0100

    g

--- apps/e2e.spec.ts
+++ apps/e2e.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-var-requires */
 import { Test, TestingModule } from '@nestjs/testing';
 import { HttpStatus, INestApplication } from '@nestjs/common';
