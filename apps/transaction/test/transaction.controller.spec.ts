7f5d225f2a37df1c4c513ac16b8536419c859136
hectorocanaperez <102577133+hectorocanaperez@users.noreply.github.com>
2022-12-20 11:42:40 +0100

    asas

---
+++ apps/transaction/test/transaction.controller.spec.ts
@@ -0,0 +1,60 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import {ApicurioSchemaService} from '../../../apicurioSchema/apicurio.service'
+import { TransactionsController } from '../src/transaction/transaction.controller';
+import { TransactionsService } from '../src/transaction/transaction.service';
+
+describe('PruebaController', () => {
+  //let Appcontroller: AppController;
+  let transaction: TransactionsController;
+  let service:TransactionsService;
+  
+const mockerTransaction = {
+    create: jest.fn((TransactionDto) => { 
+      return {
+        customId: "222-fff",
+        transactionId: '',
+        process: false,
+        tipo:"status changed",
+        time: '2022-10-17 10:09:08.607381',
+        data:{},
+        
+        ...TransactionDto, 
+      };
+    }),
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({ 
+      controllers: [TransactionsController],
+      providers: [TransactionsService,ApicurioSchemaService] ,  
+    })
+    .overrideProvider(TransactionsService)
+    .useValue(mockerTransaction)
+
+   
+    .compile();
+
+    //Appcontroller = module.get<AppController>(AppController); 
+    transaction=module.get<TransactionsController>(TransactionsController)
+    //service=module.get<TransactionsService>('TRANS_SERVICE')
+  });
+
+
+  it('should be defined', () => {
+    expect(transaction).toBeDefined();
+  });
+  
+  it ('crea una transaction',()=>{
+    expect(transaction.createTransaction({ 
+      transactionId:"",
+      process:false,
+      data:{},
+      flowId:"17d41e84-9dac-4ff3-8f86-84af998c9e8b",
+    customId:"111-bgbgb",
+    time:"2022-10-17 10:09:08.607381",
+   
+  })
+    
+  )})
+  
+});
